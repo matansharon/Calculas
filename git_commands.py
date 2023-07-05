883e73a5b2a660ea9ea8307d20c3b5149dc8932a
matansharon <matca2953@gmail.com>
2023-07-05 07:00:30 +0300

    from mac

--- git_commands.py
+++ git_commands.py
@@ -2,5 +2,5 @@ import subprocess
 
 # subprocess.run("git status")
 subprocess.run("git add --all")
-subprocess.run('git commit -m "commit from python"')
+subprocess.run('git commit -m "commit from mac"')
 subprocess.run("git push cal_github main")
